bec286dd30c867726539dbae4bebddbafab6a39b
thomz12 <thomz12@hotmail.nl>
2016-09-20 14:13:12 +0200

    Mouse is now visible

--- Hotel/Hotel/Hotel/MainGame.cs
+++ Hotel/Hotel/Hotel/MainGame.cs
@@ -25,6 +25,7 @@ namespace Hotel
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             Window.Title = "Blue Hotel";
+            IsMouseVisible = true;
         }
 
         /// <summary>
