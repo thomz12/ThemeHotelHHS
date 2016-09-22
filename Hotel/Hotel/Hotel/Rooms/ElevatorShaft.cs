70f64c7c2999b07c7ac7fa01734a8d3a70119fc1
D Inbound <D_Inbound@hotmail.com>
2016-09-22 09:31:14 +0200

    changes to UML, added elevatorshaft class.

---
+++ Hotel/Hotel/Hotel/Rooms/ElevatorShaft.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Hotel
+{
+    public class ElevatorShaft : Room
+    {
+        public ElevatorShaft(ContentManager content)
+        {
+            
+        }
+    }
+}
