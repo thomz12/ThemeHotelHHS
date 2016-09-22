4cc693af1532fc78e2484c9c7123455958d9551b
D Inbound <D_Inbound@hotmail.com>
2016-09-22 09:38:26 +0200

    Added constructor to elevatorshaft added comments to room moved one item in uml

--- Hotel/Hotel/Hotel/Rooms/ElevatorShaft.cs
+++ Hotel/Hotel/Hotel/Rooms/ElevatorShaft.cs
@@ -10,7 +10,11 @@ namespace Hotel
 {
     public class ElevatorShaft : Room
     {
-        public ElevatorShaft(ContentManager content)
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="content">The content manager used to load in images</param>
+        public ElevatorShaft(ContentManager content) : base(content)
         {
             
         }
