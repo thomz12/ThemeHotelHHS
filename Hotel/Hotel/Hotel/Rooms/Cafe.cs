cecfaa3554fcb6a43a446ee4be7b1d240b74649e
D Inbound <D_Inbound@hotmail.com>
2016-09-23 13:29:45 +0200

    Fixed an error in the new class

--- Hotel/Hotel/Hotel/Rooms/Cafe.cs
+++ Hotel/Hotel/Hotel/Rooms/Cafe.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using Hotel;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
 
 namespace Hotel.Rooms
 {
     public class Cafe : Room
     {
-        public Cafe(ContentManager content) : base(content)
+        public Cafe(ContentManager content, Point position, Point size) : base(content, position, size)
         {
             Sprite.LoadSprite("2x1Cafe");
         }
