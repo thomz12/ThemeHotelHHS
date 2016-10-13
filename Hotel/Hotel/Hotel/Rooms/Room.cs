d89b6cd9a27506bee99f761460d8c542ab276024
thomz12 <thomz12@hotmail.nl>
2016-10-13 12:11:55 +0200

    Fixed elevator going up/down at once

--- Hotel/Hotel/Hotel/Rooms/Room.cs
+++ Hotel/Hotel/Hotel/Rooms/Room.cs
@@ -177,7 +177,7 @@ namespace Hotel
             base.Draw(batch, gameTime);
 
             // if cleaning emergency, show emergency sprite
-            if (State == RoomState.Emergency)
+            if (_emergencyTime > 0)
             {
                 if ((int)_emergencyTime % 2 == 0)
                     batch.Draw(_emergencyTexture, Sprite.DrawDestination, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 1.0f);
