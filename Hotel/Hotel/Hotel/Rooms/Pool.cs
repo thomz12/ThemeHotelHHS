07b2847d19ead8cc3f61cf6d2bf942eea373be26
thomz12 <thomz12@hotmail.nl>
2016-10-25 14:09:01 +0200

    Added a pool room

---
+++ Hotel/Hotel/Hotel/Rooms/Pool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Hotel.Rooms
+{
+    public class Pool : Room
+    {
+        public Pool(int id, Point position, Point size) : base(id, position, size)
+        {
+            Sprite.LoadSprite("3x1Pool");
+            Name = "Pool";
+        }
+    }
+}
